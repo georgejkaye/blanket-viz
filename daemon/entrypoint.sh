44467f6ff422de1cbc87e840faf175e90d1212f4
George Kaye <georgejkaye@gmail.com>
2023-12-20 11:01:55 +0000

    [env] Make daemon print to docker logs

--- daemon/entrypoint.sh
+++ daemon/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+# Make sure cron can see environment variables
 printenv >> /etc/environment
+
+# Set up stdour and stderr for docker logs
+mkfifo /tmp/stdout /tmp/stderr
+chmod 0666 /tmp/stdout /tmp/stderr
+tail -f /tmp/stdout &
+tail -f /tmp/stderr >&2 &
+
 cron -f
